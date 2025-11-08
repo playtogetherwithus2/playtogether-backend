09d77bcc9d96a3b8d08d7114dd49822d6b24e472
playtogetherwithus2 <playtogetherwithus2@gmail.com>
2025-11-08 15:12:11 +0530

    updated env path

--- config/config.go
+++ config/config.go
@@ -14,11 +14,11 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	err := godotenv.Load("C:/Users/navii/Desktop/Playtogether App/playtogether-backend/.env")
-	fmt.Println(err);
-	if err != nil {
-		log.Println("Error loading .env file, using system environment variables")
-	}
+	_ = godotenv.Load(".env")
+	// fmt.Println(err);
+	// if err != nil {
+	// 	log.Println("Error loading .env file, using system environment variables")
+	// }
 
 	return &Config{
 		Port:               getEnv("PORT", "8080"),
